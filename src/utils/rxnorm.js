47311fe68953473187893ea5b8baec11e739cfdc
Paul Cyca <pecyca@gmail.com>
2025-07-16 17:04:14 -0400

    Add RxImage integration and updates

--- src/utils/rxnorm.js
+++ src/utils/rxnorm.js
@@ -1,21 +1,21 @@
-// src/utils/rxnorm.js
-export async function getRxcuiFromNdc(ndc) {
-	const formatted = ndc.replace(/\D/g, "").padStart(11, "0");
-	const dashed = `${formatted.slice(0, 5)}-${formatted.slice(5, 9)}-${formatted.slice(9)}`;
-	const res = await fetch(`https://rxnav.nlm.nih.gov/REST/ndcproperties.json?id=${dashed}&idtype=NDC`);
-	const json = await res.json();
-	return json?.ndcPropertyList?.ndcProperty?.[0]?.rxcui || null;
+export async function getRxCui(ndc) {
+	const response = await fetch(`https://rxnav.nlm.nih.gov/REST/rxcui.json?id=${ndc}&idtype=NDC`);
+	const data = await response.json();
+	return data.idGroup?.rxnormId?.[0] || null;
 }
 
-export async function getBrandName(rxcui) {
-	const res = await fetch(`https://rxnav.nlm.nih.gov/REST/rxcui/${rxcui}/related.json?tty=BN`);
-	const json = await res.json();
-	const props = json?.relatedGroup?.conceptGroup?.[0]?.conceptProperties;
-	return props?.[0]?.name || "Unknown";
+export async function getBrandName(rxCui) {
+	const response = await fetch(`https://rxnav.nlm.nih.gov/REST/rxcui/${rxCui}/related.json?tty=BN`);
+	const data = await response.json();
+	return data?.relatedGroup?.conceptGroup?.[0]?.conceptProperties?.[0]?.name || 'Unknown';
 }
 
-export async function getFormDescription(rxcui) {
-	const res = await fetch(`https://rxnav.nlm.nih.gov/REST/rxcui/${rxcui}/properties.json`);
-	const json = await res.json();
-	return json?.properties?.name || "Unknown";
+export async function getFormDescription(rxCui) {
+	const response = await fetch(`https://rxnav.nlm.nih.gov/REST/rxcui/${rxCui}/properties.json`);
+	const data = await response.json();
+	return data?.properties?.dosageForm || 'Unknown';
+}
+
+export async function isRxCuiMatch(rxCui1, rxCui2) {
+	return rxCui1 && rxCui2 && rxCui1 === rxCui2;
 }
